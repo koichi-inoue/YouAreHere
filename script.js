2fedd28d9635731ae2b62421172a17d44e50b341
koichi-inoue <inoue.ko@gmail.com>
2023-08-29 15:45:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,58 @@
+var map;
+
 var longitude;
 var latitude;
 var altitude;
 var heading;
 var speed;
-var geoLocationOptions;
-var watchID;
 
 var messageElement;
 var timeInfo;
 
-function StampTime(){
-	var t = new Date();
-	var hh = t.getHours(); if(hh<10){ hh = "0"+hh;}
-	var mm = t.getMinutes(); if(mm<10){ mm = "0"+mm;}
-	var ss = t.getSeconds(); if(ss<10){ ss = "0"+ss;}
-	return hh+ ':' + mm + ':' + ss;
-}
+var watchID;
 
-function CheckPosition(){
+
+function WatchPosition(){
 
 	if (navigator.geolocation) {
 
 		watchID = navigator.geolocation.watchPosition(
 
 			function(position){
-				var longitudeText = position.coords.longitude + '';
-				var latitudeText = position.coords.latitude + '';
-				var altitudeText = position.coords.altitude + '';
-				var headingText = position.coords.heading + '';
-				var speedText = position.coords.speed + '';
-
-				longitude.innerText = longitudeText.substring(0, 12);
-				latitude.innerText = latitudeText.substring(0, 12);
-				altitude.innerText = altitudeText.substring(0, 12);
-				heading.innerText = headingText.substring(0, 12);
-				speed.innerText = speedText.substring(0, 12);
-
-				timeInfo.innerText = '[ ' + StampTime() + ' ]';
-
-				// div要素のid
-				var map = L.map('map');
 
 				// 中心の緯度・経度とズームレベルを指定
-				map.setView([position.coords.latitude, position.coords.longitude], 20);
-			
+				map.setView([position.coords.latitude, position.coords.longitude], 16);
 				//表示するタイルレイヤのURLとAttributionコントロールを設定
 				L.tileLayer(
 				// OpenStreetMap を利用
 				'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' , {
 					attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				}).addTo(map);
-			
+				// マーカーを表示
 				L.marker([ position.coords.latitude, position.coords.longitude ] ).addTo(map)
 				.bindPopup('You are here.')
 				.openPopup();
+
+				// テキスト情報の表示
+				var longitudeText = position.coords.longitude + '';
+				var latitudeText = position.coords.latitude + '';
+				var altitudeText = position.coords.altitude + '';
+				var headingText = position.coords.heading + '';
+				var speedText = position.coords.speed + '';
+				longitude.innerText = longitudeText.substring(0, 12);
+				latitude.innerText = latitudeText.substring(0, 12);
+				altitude.innerText = altitudeText.substring(0, 12);
+				heading.innerText = headingText.substring(0, 12);
+				speed.innerText = speedText.substring(0, 12);
 			},
 
-			function(positionError){
-				messageElement.innerText = positionError.code + ': ' + positionError.message;
-			},
+			null,
 
-			geoLocationOptions
+			{
+				enableHighAccuracy: true,
+				timeout: 3000,
+				maximumAge: 600000
+			}
 
 		);
 
@@ -68,15 +60,18 @@ function CheckPosition(){
 		window.alert("お使いの環境では Geolocation が使えません");
 	}
 
-	function clearWatchPosition() {
-    	navigator.geolocation.clearWatch(watchID);
-	}
+}
 
+function clearWatchPosition() {
+	navigator.geolocation.clearWatch(watchID);
 }
 
 window.onload = function() {
 
-	// Get Location /////////////
+	// div要素のID
+	map = L.map('map');
+
+	// テキスト要素のID
 	longitude = document.getElementById('longitude');
 	latitude = document.getElementById('latitude');
 	accuracy = document.getElementById('accuracy');
@@ -85,18 +80,7 @@ window.onload = function() {
 	heading = document.getElementById('heading');
 	speed = document.getElementById('speed');
 
-	geoLocationOptions = {
-		enableHighAccuracy: true,
-		timeout: 3000,
-		maximumAge: 600000
-	};
-
-	messageElement = document.getElementById('message');
-	
-	timeInfo = document.getElementById('timeInfo');
-	timeInfo.innerText = '[ ' + StampTime() + ' ]';
-
-	CheckPosition();
+	WatchPosition();
 
 }
 
